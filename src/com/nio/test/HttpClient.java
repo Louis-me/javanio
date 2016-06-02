c332df4a4d745b86f0eaeb51a54a9c28071079c3
284772894 <284772894@qq.com>
2016-06-02 21:20:37 +0800

    post client

--- src/com/nio/test/HttpClient.java
+++ src/com/nio/test/HttpClient.java
@@ -13,6 +13,7 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpRequestEncoder;
@@ -87,13 +88,15 @@ public class HttpClient {
             	 ChannelFuture f = b.connect(host, port).sync();
 
                URI uri = new URI("http://gc.ditu.aliyun.com:80/geocoding?a=深圳市");
-                 String msg = "Are you ok?";
-                 DefaultFullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET,
-                         uri.toASCIIString(), Unpooled.wrappedBuffer(msg.getBytes("UTF-8")));                             
+               FullHttpRequest request = new DefaultFullHttpRequest(
+                       HttpVersion.HTTP_1_1, HttpMethod.POST, uri.getRawPath());                        
                  // 构建http请求
-                 request.headers().set(HttpHeaders.Names.HOST, host);
-                 request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
-                 request.headers().set(HttpHeaders.Names.CONTENT_LENGTH, request.content().readableBytes());
+               request.headers().set(HttpHeaders.Names.HOST, host);
+               request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE); // or HttpHeaders.Values.CLOSE
+               request.headers().set(HttpHeaders.Names.ACCEPT_ENCODING, HttpHeaders.Values.GZIP);
+               request.headers().add(HttpHeaders.Names.CONTENT_TYPE, "application/json");
+               ByteBuf bbuf = Unpooled.copiedBuffer("{\"jsonrpc\":\"2.0\",\"method\":\"calc.add\",\"params\":[1,2],\"id\":1}", StandardCharsets.UTF_8);
+               request.headers().set(HttpHeaders.Names.CONTENT_LENGTH, bbuf.readableBytes());
                  // 发送http请求
                  f.channel().write(request);
                  f.channel().flush();
